ef558a02a49913d24070ccc1056ca21637b5a123
jongman <jongman@jongman-desktop.(none)>
2011-07-24 17:43:48 -0500

    fixed install.py: run from anywhere

--- install.py
+++ install.py
@@ -2,18 +2,19 @@
 import glob
 import os
 current_dir = os.path.abspath(os.path.dirname(__file__))
+j = lambda *args: os.path.join(current_dir, *args)
 
 jobs = []
 jobs += [(config, "~/.%s" % os.path.basename(config).lstrip("_"))
-         for config in glob.glob("dotfiles/_*")]
-jobs += [("bin", "~/bin")]
+         for config in glob.glob(j("dotfiles/_*"))]
+jobs += [(j("bin"), "~/bin")]
 
 for path, target in jobs:
     target = os.path.expanduser(target)
     if os.path.lexists(target):
         print target, "already exists"
     else:
-        os.symlink(os.path.join(current_dir, path), target)
+        os.symlink(path, target)
         print "created", target
 
 
