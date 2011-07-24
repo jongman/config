bc8224c59f2391f881825df105f5449159457900
jongman <jongman@jongman-desktop.(none)>
2011-07-24 17:30:54 -0500

    added cdgit to _zshrc, and PATH setting

--- install.py
+++ install.py
@@ -2,13 +2,18 @@
 import glob
 import os
 current_dir = os.path.abspath(os.path.dirname(__file__))
-for config in glob.glob("dotfiles/_*"):
-    fn = os.path.basename(config)
-    target = os.path.expanduser("~/.%s" % fn.lstrip("_"))
+
+jobs = []
+jobs += [(config, "~/.%s" % os.path.basename(config).lstrip("_"))
+         for config in glob.glob("dotfiles/_*")]
+jobs += [("bin", "~/bin")]
+
+for path, target in jobs:
+    target = os.path.expanduser(target)
     if os.path.lexists(target):
         print target, "already exists"
     else:
-        os.symlink(os.path.join(current_dir, config), target)
+        os.symlink(os.path.join(current_dir, path), target)
         print "created", target
 
 
