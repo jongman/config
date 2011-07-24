068c0ec50247c002fb01fb91d3186f14dd060354
jongman <jongman@jongman-desktop.(none)>
2011-07-24 17:20:56 -0500

    bin directory contains some homewritten scripts...

---
+++ bin/t.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+#include<cassert>
+#include<cstring>
+#include<map>
+#include<set>
+#include<time.h>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<utility>
+#include<cmath>
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main() {
+  int cases;
+  cin >> cases;
+  for(int cc = 0; cc < cases; ++cc) {
+
+  }
+}
+
